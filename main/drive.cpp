e2ad5d74227dad9f32bf39e93414a790943257c8
Tim Reddy <treddy@gmail.com>
2024-10-04 20:54:15 -0400

    Setting up and confirming build environment

--- main/drive.cpp
+++ main/drive.cpp
@@ -9,11 +9,16 @@
 #include "driver/gpio.h"
 #include "esp_log.h"
 
+#define MOTOR_PWM_TIMER_RESOLUTION_HZ 10000000 // 10MHz, 1 tick = 0.1us
+#define MOTOR_PWM_PERIOD              500      // 50us, 20KHz
+#define MOTOR_PWM_PIN1  GPIO_NUM_27
+#define MOTOR_PWM_PIN2  GPIO_NUM_15
+
 static const char* TAG = "Drive";
 
 extern "C" void app_main() {
-    ESP_LOGE(TAG, "Test starting");
-
+    ESP_LOGI(TAG, "Test starting");
+    DRV8871 motor(MOTOR_PWM_PIN1, MOTOR_PWM_PIN2);
     while(1) {
 	;
     }
